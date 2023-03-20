f193d06cf35b17df65862e2debc2fa16a1007ea1
cnichols-git <cnichols.mail@gmail.com>
2023-03-20 16:04:58 -0500

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,17 @@ provider "aws" {
   access_key = ""
   secret_key = ""
 }
+
+#tag will add metadate to id the resource instance#
+#tags = {
+#  Name = "Ubuntu"
+#}
+
+#tags can also be in the resource#
+resource "aws_instance" "devops-demo" {
+  ami = ""
+  instance_type = ""
+  tags = {
+    Name = "Ubuntu"
+  }
+}
